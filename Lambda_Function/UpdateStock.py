41b98e53ae2b2f514d60433bcbcff68f693f92b1
Puttpong Kamnerdpongpreeda - 6609650541 <145310003+puttipong-6609650541@users.noreply.github.com>
2025-05-26 15:36:57 +0700

    UpdateStockAlreadyWithSNS

--- Lambda_Function/UpdateStock.py
+++ Lambda_Function/UpdateStock.py
@@ -4,6 +4,9 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Product')
 
+sns = boto3.client('sns')
+TOPIC_ARN = 'arn:aws:sns:us-east-1:827429375794:SellWaiLowStockAlert'  # ใช้ของคุณเอง
+
 def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
@@ -16,16 +19,32 @@ def lambda_handler(event, context):
                 'body': json.dumps({'error': 'Missing or invalid data'})
             }
 
+        #อัปเดต stock
         table.update_item(
             Key={'ProductID': product_id},
             UpdateExpression='SET stock = :s',
             ExpressionAttributeValues={':s': new_stock}
         )
 
+        #ถ้า stock < 5 → แจ้งเตือน
+        if new_stock < 5:
+            # 🔍 ดึงชื่อสินค้า
+            response = table.get_item(Key={'ProductID': product_id})
+            item = response.get('Item', {})
+            product_name = item.get('name', f'ProductID {product_id}')
+
+            message = f"สินค้า {product_name} เหลือเพียง {new_stock} ชิ้นแล้ว!"
+            sns.publish(
+                TopicArn=TOPIC_ARN,
+                Message=message,
+                Subject="SellWai - แจ้งเตือนสินค้าใกล้หมดสต๊อก"
+            )
+
         return {
             'statusCode': 200,
             'body': json.dumps({'message': f'Stock updated for {product_id}'})
         }
+
     except Exception as e:
         return {
             'statusCode': 500,
